a2dc25023878f8a61e4e9de20202025e47f7135f
Jonathan Widdowson <jonathan.widdowson@gmail.com>
2020-02-01 15:07:46 +0200

    Create input output lib for print, save, page body

--- http-testing.go
+++ http-testing.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	inputoutput "http-testing/lib/lib_inputoutput"
 	requests "http-testing/lib/lib_requests"
 )
 
 func main() {
 	url := "https://news.ycombinator.com/item?id=22201337"
+	inputoutput.WriteToFile("test", requests.GetPageResponseString(url))
 }
