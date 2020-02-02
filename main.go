943553ea9777d622626b7c7fefe5119c0fd4eed5
Jonathan Widdowson <jonathan.widdowson@gmail.com>
2020-02-02 16:47:57 +0200

    Remove http-testing.go, replaced by main.go

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
-import (
-	lib "http-testing/lib"
-)
+import "http-testing/lib"
 
 func main() {
-	url := "https://news.ycombinator.com/item?id=22201337"
-	lib.WriteToFile("test", lib.GetPageResponseString(url))
+	url := "https://news.ycombinator.com/item?id=22213210"
+	lib.WriteToFile("test", lib.PageString(url))
+	lib.GetNodeContent(lib.PageString(url), "p")
 }
