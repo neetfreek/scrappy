9a211ce9c0a7b8706d3c5ba8538003bf43336491
Jonathan Widdowson <jonathan.widdowson@gmail.com>
2020-02-09 20:54:34 +0200

    Add cases for token-based parsing

--- lib/page_parsing.go
+++ lib/page_parsing.go
@@ -1,65 +1,15 @@
 package lib
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"strings"
 
 	"golang.org/x/net/html"
 )
 
-// GetNodeContent gets specified node content
-func GetNodeContent(pageResponseString, tag string) {
-
-	// doc is *html.Node which can be crawled. Here it's parsed using golang's /x/net/html func Parse example for links
-	doc, err := html.Parse(strings.NewReader(pageResponseString))
-	if err != nil {
-		log.Fatal(err)
-	}
-	crawlNodes(tag, doc)
-}
-
-func crawl(n *html.Node) {
-	// n.Data refers to tag name for element nodes, content for text
-	if n.Type == html.ElementNode && n.Data == "a" {
-		for _, a := range n.Attr {
-			if a.Key == "href" {
-				fmt.Println(a.Val)
-				break
-			}
-		}
-	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		crawl(c)
-	}
-}
-
-func renderNode(n *html.Node) string {
-	var buf bytes.Buffer
-	w := io.Writer(&buf)
-	html.Render(w, n)
-	return buf.String()
-}
-
-func crawlNodes(tag string, n *html.Node) {
-
-	if n.Type == html.ElementNode && n.Data == tag {
-		fmt.Printf("\nNODE ALL: %s\n", renderNode(n))
-		fmt.Printf("NODE DATA: %s\n", n.Data)            // node tag
-		fmt.Printf("NODE DATA_ATOM: %s\n", n.DataAtom)   // node attr
-		fmt.Printf("NODE 1ST_CHILD: %v\n", n.FirstChild) // node 1st_child **
-		fmt.Printf("NODE TYPE: %v\n", n.Type)            // node type
-	}
-
-	for childNode := n.FirstChild; childNode != nil; childNode = childNode.NextSibling {
-		crawlNodes(tag, childNode)
-	}
-}
-// Tester tests
-func Tester(url string) {
+// PrintAttribute for testing print http tag attributes to standard output
+func PrintAttribute(url string) {
 	// Get response
 	resp, err := http.Get(url)
 	if err != nil {
@@ -76,31 +26,26 @@ func Tester(url string) {
 		switch responsToken {
 		case html.ErrorToken:
 			return
+		//For working with tags, e.g. inspecting tag values or attributes
 		case html.StartTagToken:
-			// fmt.Printf("\n%v\n", responsToken)
 			tag := responseToken.Token()
 			tagType := tag.Data
-			tokenType(tagType)
-			// fmt.Printf("\n%v\n", tag.Data)
+			fmt.Print("\nStartTagToken tag: ", tag)
+			fmt.Print("\ntagType: ", tagType)
+			// if (IdentifyTag(tagType)) != "" {
+			// 	for _, attribute := range tag.Attr {
+			// 		fmt.Print("\n tagAttribute: ", attribute.Val)
+			// 		break
+			// 	}
+			// }
+		case html.TextToken:
+			tag := responseToken.Token()
+			fmt.Print("\nTextToken tag: ", tag)
+			// if (IdentifyTag(tag.String())) == HTMLTags.tagParagrah {
+			// 	fmt.Print(tag, "\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-			// Print href links (url and image links)
-			for _, a := range tag.Attr {
-				if a.Key == "href" {
-					fmt.Println("Found :", a.Val)
-					break
-				}
-			}
+			// }
+			// fmt.Print(tag, "\n")
 		}
-	}
-}
-
-func tokenType(tokenType string) {
-	switch tokenType {
-	case "p":
-		fmt.Print("PARAGRAPH")
-	case "a":
-		fmt.Print("ANCHOR")
-	default:
-		fmt.Print("SOME OTHER TAG TYPE")
 	}
 }
