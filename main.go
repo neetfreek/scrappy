6649f839e3aae85e946940d2f66e31fff638b82b
Jonathan Widdowson <jonathan.widdowson@gmail.com>
2020-02-12 19:03:23 +0200

    Update main() calls

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
-import "http-testing/lib"
+import (
+	"http-testing/lib"
+)
 
 func main() {
 	// url := "https://news.ycombinator.com/item?id=22213210"
 	url := "https://www.neetfreek.net"
-	// lib.WriteToFile("test", lib.PageString(url))
-	// lib.WriteToFile("test", lib.PageString(url))
-	lib.Tester(url)
+	lib.SetupTags()
+	lib.PrintAttribute(url)
 }
