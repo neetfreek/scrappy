d7e6a5ffab16d9fb21262a2c8100622dbef7fda5
Jonathan Widdowson <jonathan.widdowson@gmail.com>
2020-02-14 20:05:08 +0200

    Update to use options array instead of map

--- lib/userInteraction.go
+++ lib/userInteraction.go
@@ -15,15 +15,15 @@ func showInstruction() {
 }
 
 func showUserInputOptions() {
-	for optionNumber, optionString := range InputOptionsMap {
-		fmt.Printf("%v. %v\n", optionNumber, optionString)
+	for counter := range InputOptionsMap {
+		fmt.Printf("%v. %v\n", counter+1, InputOptionsMap[counter])
 	}
 }
 
 func getUserInputOption() int {
 	userInput := 1
 	fmt.Println("Get user input, for now set to 1")
-	// Read user input as int, assign to var
+	// Read user input as int, decrement by one, assign to var
 	// if error, call showUserInputOptions
 	// Return user input variable
 	return userInput
